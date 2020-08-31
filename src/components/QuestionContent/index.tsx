1b4df0dc846fe86be0b00a9c15cd96b637f3fa11
thlindustries <thlindustries23@gmail.com>
2020-08-31 14:17:12 -0300

    Chore: removing console.logs of content component

--- src/components/QuestionContent/index.tsx
+++ src/components/QuestionContent/index.tsx
@@ -12,24 +12,20 @@ const QuestionContent: React.FC<ContentProps> = ({ type, url }) => {
   const [winWidth, setWinWidth] = useState(0);
 
   const showCurrentTime = useCallback((info: any) => {
-    console.log('Video pausado');
-    console.log(info);
+    // console.log('Video pausado');
+    // console.log(info);
   }, []);
 
   const handleEndVideo = useCallback((info: any) => {
-    console.log('Video terminado');
-    console.log(info);
+    // console.log('Video terminado');
+    // console.log(info);
   }, []);
 
-  const handleProgressVideo = useCallback(
-    (info: any) => {
-      console.log('Video rodando');
-      console.log(`largura da tela: ${winWidth}`);
-
-      console.log(info);
-    },
-    [winWidth],
-  );
+  const handleProgressVideo = useCallback((info: any) => {
+    // console.log('Video rodando');
+    // console.log(`largura da tela: ${winWidth}`);
+    // console.log(info);
+  }, []);
 
   useEffect(() => {
     const width = window.innerWidth;
@@ -59,6 +55,7 @@ const QuestionContent: React.FC<ContentProps> = ({ type, url }) => {
           <ReactAudioPlayer
             src="https://audiochicoteste.s3-sa-east-1.amazonaws.com/audios/Lil+Nas+X+-+Old+Town+Road+(adamsteve+Remix).mp3"
             controls
+            volume={0.5}
           />
         </>
       )}
