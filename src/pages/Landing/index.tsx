aae1f9f86c890f508a59b97220d19a3c5beaa494
thlindustries <thlindustries23@gmail.com>
2020-09-29 17:05:32 -0300

    Feature: loading in test button

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import ReactLoading from 'react-loading';
 // import { Link } from 'react-router-dom';
 import Axios from 'axios';
 
@@ -26,12 +27,14 @@ const Landing: React.FC = () => {
   const countDownDate = new Date('October 8, 2020').getTime();
 
   const [tab, setTab] = useState('');
+  const [isLogging, setIsLogging] = useState(false);
 
   useEffect(() => {
     setTab('home');
   }, []);
 
   const handleTest = useCallback(async () => {
+    setIsLogging(true);
     const response = await Axios.get<ResponseLoginFake>(
       'https://16hgpfnq69.execute-api.sa-east-1.amazonaws.com/prod/loginfake',
     );
@@ -57,7 +60,9 @@ const Landing: React.FC = () => {
               <form action="https://docs.google.com/forms/d/e/1FAIpQLSeA_fLgG3Sk9sEHDK6R74i0-ePBeeNk_6y7ZpIxlIwiGaF_bA/viewform">
                 <ButtonSubscribe type="submit">Inscrever-se</ButtonSubscribe>
               </form>
-              <ButtonSubscribe onClick={handleTest}>Testar</ButtonSubscribe>
+              <ButtonSubscribe onClick={handleTest}>
+                {isLogging ? <ReactLoading /> : 'Testar'}
+              </ButtonSubscribe>
             </ButtonsContainer>
           </CountButton>
         </Content>
