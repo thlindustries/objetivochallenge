3bafee9b29eeb957e851fb4003420630fe45154b
thlindustries <thlindustries23@gmail.com>
2020-09-30 09:56:39 -0300

    Fix: fixign enviroment variables

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -48,7 +48,7 @@ export const AuthProvider: React.FC = ({ children }) => {
 
   const signIn = useCallback(async ({ email, password }) => {
     const response = await Axios.post(
-      `https://16hgpfnq69.execute-api.sa-east-1.amazonaws.com/dev/login`,
+      `${process.env.REACT_APP_DEV_API}/login`,
       {
         UserEmail: email,
         UserPassword: password,
